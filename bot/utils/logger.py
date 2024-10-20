761c1168c011b0e5976081fada1b72211956e8d1
duonanotte <166713796+duonanotte@users.noreply.github.com>
2024-10-20 12:42:09 +0300

    Update logger.py

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -6,7 +6,7 @@ logger.remove()
 logger.add(
     sink=sys.stdout,
     format=(
-        "<green><b>[BLUM]</b></green> "
+        "<blue><b>[BLUM]</b></blue> "
         "| <white>{time:HH:mm:ss}</white> "
         "| <level>{level: <8}</level> "
         "| <white><b>{message}</b></white>"
